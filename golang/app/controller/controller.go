d1f0c25a4afc13fd144305621db55de4662b6bdb
Senritsu420 <kubo3900@gmail.com>
2023-11-11 16:48:57 +0900

    Docker情報更新＆APIのPOST,PUT,DELETE追加

--- golang/app/controller/controller.go
+++ golang/app/controller/controller.go
@@ -8,37 +8,70 @@ import (
 	"github.com/labstack/echo"
 )
 
-// レスポンスJsonの定義
-type RequestJson struct {
-	Id   int    `json:"int"`
-	Name string `json:"name"`
-	Age  int    `json:"age"`
-}
-
-type ResponseAll struct {
-	Status string      `json:"status"`
-	Data   []user.User `json:"data"`
-}
-
-type ResponseOne struct {
-	Status string    `json:"status"`
-	Data   user.User `json:"data"`
-}
-
+// 全てのuser情報を取得し、JSON形式で返す
 func GetAllUser(c echo.Context) error {
+	// 全てのuser情報を取得
 	users := user.ReadAll(user.Db)
-	return c.JSON(http.StatusOK, ResponseAll{
-		Status: "OK",
-		Data:   users,
-	})
+	// 全てのuser情報をJSON形式で返す
+	return c.JSON(http.StatusOK, users)
 }
 
+// 特定のuser情報を取得し、JSON形式で返す
 func GetOneUser(c echo.Context) error {
+	// パスパラメータ「id」を取得
 	param := c.Param("id")
+	// idを整数型に変換
 	id, _ := strconv.Atoi(param)
+	// 特定のuser情報を取得
 	user := user.ReadOne(user.Db, id)
-	return c.JSON(http.StatusOK, ResponseOne{
-		Status: "OK",
-		Data:   user,
-	})
+	// 特定のuser情報をJSON形式で返す
+	return c.JSON(http.StatusOK, user)
+}
+
+// 送られてきた情報を元に新規userを作成し、JSON形式で返す
+func PostNewUser(c echo.Context) error {
+	// user構造体作成
+	u := new(user.User)
+	// 送られてきた情報を元に構造体へのデータ格納を行う
+	err := c.Bind(u)
+	// エラー発生時はエラーを返す
+	if err != nil {
+		return err
+	}
+	// 構造体の情報を元に新規userを作成
+	user.InsertUser(user.Db, u.Name, u.Age)
+	// 新規user情報をJSON形式で返す
+	return c.JSON(http.StatusOK, u)
+}
+
+// 送られてきた情報を元にuser情報を更新し、JSON形式で返す
+func PutUser(c echo.Context) error {
+	// パスパラメータ「id」を取得
+	param := c.Param("id")
+	// idを整数型に変換
+	id, _ := strconv.Atoi(param)
+	// user構造体作成
+	u := new(user.User)
+	// 送られてきた情報を元に構造体へのデータ格納を行う
+	err := c.Bind(u)
+	// エラー発生時はエラーを返す
+	if err != nil {
+		return err
+	}
+	// 構造体の情報を元にuserを更新
+	user.UpdateUser(user.Db, id, u.Name, u.Age)
+	// 更新したuser情報をJSON形式で返す
+	return c.JSON(http.StatusOK, u)
+}
+
+// 送られてきた情報を元にuserを削除し、JSON形式で返す
+func DeleteUser(c echo.Context) error {
+	// パスパラメータ「id」を取得
+	param := c.Param("id")
+	// idを整数型に変換
+	id, _ := strconv.Atoi(param)
+	// 構造体の情報を元にuserを更新
+	user := user.DeleteUser(user.Db, id)
+	// 更新したuser情報をJSON形式で返す
+	return c.JSON(http.StatusOK, user)
 }
--- golang/app/main.go
+++ golang/app/main.go
@@ -2,21 +2,40 @@ package main
 
 import (
 	"app/controller"
+	"net/http"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
 
 func main() {
+	// echoインスタンスを生成
 	e := echo.New()
-
+	// httpリクエストの情報をログに表示
 	e.Use(middleware.Logger())
+	// パニックを回復し、スタックトレースを表示
 	e.Use(middleware.Recover())
 
+	/* CORS(オリジン間リソース共有)の設定 */
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		/* http://localhost:3000からの接続を許可する */
+		AllowOrigins: []string{"http://localhost:3000"},
+		AllowMethods: []string{
+			http.MethodGet,
+			http.MethodPut,
+			http.MethodPost,
+			http.MethodDelete,
+		},
+	}))
+
+	// ルーティング
 	e.GET("/users", controller.GetAllUser)
-	// e.POST("/users")
+	e.POST("/users", controller.PostNewUser)
 	e.GET("/users/:id", controller.GetOneUser)
-	// e.PUT("/users/:id")
-	// e.DELETE("/users/:id")
+	e.PUT("/users/:id", controller.PutUser)
+	e.DELETE("/users/:id", controller.DeleteUser)
+
+	// サーバーをスタートさせる
+	// ポート番号は引数で指定できる
 	e.Logger.Fatal(e.Start(":8080"))
 }
